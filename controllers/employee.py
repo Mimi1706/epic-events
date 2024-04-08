0607cdd9a0087925f55d968b046d6595eabe8436
Mimi <hana.nguyen@outlook.fr>
2024-04-08 14:33:53 +0200

    Updating employees display + adding employee search

--- controllers/employee.py
+++ controllers/employee.py
@@ -1,5 +1,6 @@
 from views.employee import EmployeeView
 from models import Employee
+from typing import List
 from database import session
 from permissions import EMPLOYEE_PERMISSIONS
 
@@ -16,7 +17,7 @@ class EmployeeController:
             if user_input == "1" and 'READ' in allowed_actions:
                 self.read_employees()
             elif user_input == "2" and 'READ' in allowed_actions:
-                print('recherche autorisée')
+                self.find_employee()
             elif user_input == "3" and 'CREATE' in allowed_actions:
                 print('création autorisée')
             elif user_input == "4" and 'UPDATE' in allowed_actions:
@@ -27,8 +28,17 @@ class EmployeeController:
                 quit()
 
     def read_employees(self):
-        employees = session.query(Employee).all()
-        print("\nID | Name")
-        print("-------------------")
-        for employee in employees:
-            print(f"{employee.id}  | {employee.full_name}")
+        employees: List[Employee] = session.query(Employee).all()
+        sorted_employees: List[Employee] = sorted(employees, key=lambda employee:employee.department)
+        self.view.display_employee_header()
+        for employee in sorted_employees:
+            self.view.display_employee(employee)
+
+    def find_employee(self):
+        user_input = self.view.find_employee()
+        employee = session.query(Employee).filter(Employee.id == user_input).first()
+        if employee:
+            self.view.display_employee_header()
+            return self.view.display_employee(employee)
+        else:
+            return self.view.employee_not_found()
--- views/employee.py
+++ views/employee.py
@@ -6,20 +6,30 @@ class EmployeeView:
         role_permissions = EMPLOYEE_PERMISSIONS.get(employee.department, {})
         allowed_actions = [action for action, allowed in role_permissions.items() if allowed]
         menu_options = ["\n1 - Voir la liste des utilisateurs"]
-
         if 'READ' in allowed_actions:
             menu_options.append("2 - Chercher un utilisateur")
-
         if 'CREATE' in allowed_actions:
             menu_options.append("3 - Créer un utilisateur")
-
         if 'UPDATE' in allowed_actions:
             menu_options.append("4 - Mettre à jour un utilisateur")
-
         if 'DELETE' in allowed_actions:
             menu_options.append("5 - Effacer un utilisateur")
-
         menu_options.append("6 - Retour\n")
-
         return input("\n".join(menu_options))
+    
+    def find_employee(self):
+        return input("\nVeuillez entrer le numéro ID de l'utilisateur: ")
+    
+    def display_employee_header(self):
+        min_widths = [3, 4, 12]
+        employee_header = f"\nID{min_widths[0]*" "} | Département{min_widths[1]*" "} | Nom{min_widths[2]*" "} | Email{min_widths[2]*" "}"
+        print(employee_header)
+        print("-" * len(employee_header))
+    
+    def display_employee(self, employee: Employee) -> None:
+        min_widths = [5, 15]
+        print(f"{employee.id:<{min_widths[0]}} | {employee.department:<{min_widths[1]}} | {employee.full_name:<{min_widths[1]}} | {employee.email}")
+
+    def employee_not_found(self):
+        return print("Utilisateur non trouvé")
 
