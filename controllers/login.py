46d169cb8cabee4bf77d0d77dadb0f3a97e885a7
Mimi <hana.nguyen@outlook.fr>
2024-04-28 18:50:32 +0200

    Adding token saving and logging

--- controllers/login.py
+++ controllers/login.py
@@ -13,16 +13,21 @@ class Login:
         self.view = LoginView()
 
     def log_in(self):
+        token = self.load_token()
+        payload = self.validate_token(token)
+        if token and payload:
+            employee = session.query(Employee).filter(Employee.id == payload["id"]).first()
+            if employee:
+                return self.generate_jwt(employee)
+            
         username = self.view.get_username()
         password = self.view.get_password().encode('utf-8')
         employee = session.query(Employee).filter(Employee.email == username).first()
+        if employee and bcrypt.checkpw(password, employee.password.encode('utf-8')):
+            return self.generate_jwt(employee)
 
-        if employee:
-            employee_password = employee.password.encode('utf-8')  
-            if bcrypt.checkpw(password, employee_password):
-                token = self.generate_jwt(employee)
-                return token
         return None
+
     
     def generate_jwt(self, employee:Employee):
         payload = {
@@ -30,6 +35,7 @@ class Login:
         'token_expiration': (datetime.now() + timedelta(hours=8)).isoformat()
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        self.save_token(token)
         return token
     
     def authorize_user(self, token):
@@ -44,3 +50,30 @@ class Login:
         except jwt.InvalidTokenError:
             self.view.error_msg()
             quit()
+
+    def save_token(self, token):
+        file_path = "token.txt"
+        encoded_token = token.encode('utf-8') 
+        with open(file_path, "wb") as file: 
+            file.write(encoded_token)
+
+    def load_token(self):
+        file_path = "token.txt"
+        try:
+            with open(file_path, "rb") as file: 
+                encoded_token = file.read()
+                return encoded_token.decode('utf-8')  
+        except FileNotFoundError:
+            pass
+        return None
+
+    def validate_token(self, token):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            expiration = datetime.fromisoformat(payload['token_expiration'])
+            if datetime.now() < expiration:
+                return payload
+            else:
+                return None
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            return False 
