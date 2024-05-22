23adf48ba57ea5b18c44f69a3ad21f32d7eb94ee
Mimi <hana.nguyen@outlook.fr>
2024-05-23 01:32:44 +0200

    Fixing login tests + fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from models.base import Base  
 from models.employee import Employee  
 import os
 from dotenv import load_dotenv
@@ -9,29 +8,24 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DB_URL")
 
-@pytest.fixture(scope='module')
-def db_engine():
+@pytest.fixture(scope='function')
+def mock_db_session():
     engine = create_engine(DATABASE_URL)
-    Base.metadata.create_all(engine)
-    yield engine
-    Base.metadata.drop_all(engine)
-
-@pytest.fixture(scope='function')
-def db_session(db_engine):
-    Session = sessionmaker(bind=db_engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    yield session
-    session.close()
+    session.begin()
+    yield session 
+    session.rollback()
+    session.close() 
 
 @pytest.fixture(scope='function')
-def generate_employee(db_session: Session):
+def mock_employee(mock_db_session: Session):
     employee = Employee(
         email='jane@email.fr', 
         password='$2b$12$eb0YquWvV.QXxbmxbve4XePjFjOXm6/n0qL1tpB2vjFOKvRRFvV8W', 
         full_name='Jane Doe',
         department="gestion"
     )
-    db_session.add(employee)
-    db_session.commit()
+    mock_db_session.add(employee)
+    mock_db_session.commit()
     yield employee
-    db_session.rollback()
--- tests/unit_tests/test_login.py
+++ tests/unit_tests/test_login.py
@@ -2,8 +2,8 @@ from controllers.login import Login
 from models.employee import Employee
 from pytest_mock import MockerFixture
 
-def test_login(generate_employee: Employee, mocker: MockerFixture):
-    test_email = generate_employee.email
+def test_login(mock_employee: Employee, mocker: MockerFixture):
+    test_email = mock_employee.email
     mocker.patch('builtins.input', side_effect=[test_email, "1"])
     login = Login().log_in()
     assert login is not None
